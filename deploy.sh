8a90077c3fb1d32fda7b29436e8a9d2e28793881
Zach <zpg6@pitt.edu>
2020-09-10 17:05:41 -0400

    Fixing errors with deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+echo -n "\nEnter your dev branch: \n"
+read devBranch
+echo -n "\nEnter your commit message: \n"
+read commitMessage
+
 rm -rf public/*
 ng build
 cp -rf dist/pingg/* public
 firebase deploy --only hosting
+git add .
+git commit -m "${commitMessage}"
+git push origin
+git checkout master
+git merge ${devBranch}
+git push origin
+git checkout ${devBranch}
+
 
 
 
