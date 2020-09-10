f277d8b30b8c2e3bc93455dd3f7113973fa0ac49
Zach <zpg6@pitt.edu>
2020-09-10 17:32:15 -0400

    Practicing branching

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
 
-echo -n "\nEnter your dev branch: \n"
+echo -n "Enter your dev branch: "
 read devBranch
-echo -n "\nEnter your commit message: \n"
+echo -n "Enter your commit message: "
 read commitMessage
 
 rm -rf public/*
 ng build
 cp -rf dist/pingg/* public
 firebase deploy --only hosting
-git add -A
+
+git add .
 git commit -m "${commitMessage}"
-git push origin ${devBranch}
 git checkout master
 git pull origin master
 git merge ${devBranch}
-<<<<<<< HEAD
 git add .
 git commit -m "${commitMessage}"
-=======
->>>>>>> zach
 git push origin master
 git checkout ${devBranch}
 git merge master
-git push origin
+git push origin ${devBranch}
 
 
 
