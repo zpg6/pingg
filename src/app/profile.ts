a6655aec6aa09ccdd53b488838e9b7ac3c489336
Joe Paul <jjp106@pitt.edu>
2020-10-13 16:59:42 -0400

    added in profile class

---
+++ src/app/profile.ts
@@ -0,0 +1,10 @@
+export class Profile {
+    id: string = "";
+    currentPing: number = 0;
+    city: string = "";
+    state: string = "";
+    firstName: string = "";
+    lastName: string = "";
+    handle: string = "";
+    screenNames: {} = {};
+}
